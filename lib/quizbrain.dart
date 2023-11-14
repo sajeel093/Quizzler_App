8170528bcb28b8e6324e76b3540f0caaa1a246c9
sajeel093 <sajeelhassan53@gmail.com>
2023-11-15 04:25:35 +0500

    complete app

---
+++ lib/quizbrain.dart
@@ -0,0 +1,71 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _qnumber = 0;
+  final List<Questions> _bank = [
+    Questions(
+      'you are a noob',
+      false,
+    ),
+    Questions(
+      'you are not a noob',
+      true,
+    ),
+    Questions(
+      'what should we do? eat?',
+      true,
+    ),
+    Questions('You can lead a cow down stairs but not up stairs.', false),
+    Questions(
+        'Approximately one quarter of human bones are in the feet.', true),
+    Questions('A slug\'s blood is green.', true),
+    Questions('Buzz Aldrin\'s mother\'s maiden name was', true),
+    Questions('It is illegal to pee in the Ocean in Portugal.', true),
+    Questions(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Questions(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Questions(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Questions(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Questions('Google was originally called .', true),
+    Questions(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Questions(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  void nextQuestion() {
+    if (_qnumber < _bank.length - 1) {
+      _qnumber++;
+    }
+  }
+
+  String getQuestion() {
+    return _bank[_qnumber].questionText;
+  }
+
+  bool getAnswer() {
+    return _bank[_qnumber].questionAnswer;
+  }
+
+  bool isFinished() {
+    if (_qnumber >= _bank.length - 1) {
+      print('Now returning true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _qnumber = 0;
+  }
+}
